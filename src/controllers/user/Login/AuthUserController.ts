6e1a00f08e0a4a7dd8d8df8fcb937210873ee598
carol <carool@live.com>
2022-05-12 17:06:07 -0300

    feat: add feature Login User

---
+++ src/controllers/user/Login/AuthUserController.ts
@@ -0,0 +1,20 @@
+import { AuthUserService } from "../../../sevices/user/Login/AuthUserService";
+import { Request, Response } from 'express';
+
+class AuthUserController {
+    async handle(req: Request, res: Response) {
+        const { email, password } = req.body;
+
+        const authUserService = new AuthUserService();
+
+        const auth = await authUserService.execute({
+            email,
+            password
+        })
+
+        return res.json(auth);
+
+    }
+}
+
+export { AuthUserController }
